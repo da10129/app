9a821389da4e11537af7641434b20e800d46c84c
da10129 <119820010+da10129@users.noreply.github.com>
2022-12-06 07:21:26 +0900

    importAuth-1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { Amplify } from 'aws-amplify';
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import config from './aws-exports';
-Amplify.configure(config);
 
-function App({signOut,user}) {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       {user &&
-          <div>
-            {user.username}
-            <button onClick={signOut}>
-              ログアウト
-            </button>
-          </div>
-        }
+        <h2>Hello React with AWS</h2>
+        {user ? (
+          <>
+            <h3>私は権限を持っています：{user.username}</h3>
+            <button onClick={signOut}>Sign out</button>
+          </>
+        ) : (
+          <h3>権限がありません</h3>
+        )}
       </header>
     </div>
   );
